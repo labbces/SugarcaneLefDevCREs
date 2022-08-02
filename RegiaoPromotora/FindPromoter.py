c132fd42c8429c5c40f9b5c5f88bde8e274d8bbf
JoaoNovoletti <joao.novoletti@usp.br>
2022-08-02 19:15:23 -0300

    atualizando FindPromoter

--- RegiaoPromotora/FindPromoter.py
+++ RegiaoPromotora/FindPromoter.py
@@ -4,15 +4,31 @@ import argparse
 import pyranges as pr
 import pandas as pd
 
-
 parser= argparse.ArgumentParser(description='Insert the lenth of the Expected Promoter')
-parser.add_argument('integers', metavar='N', type=int,
-                            help='insert the lenth of the Pormoter')
-Promoter = parser.parse_args()
+parser.add_argument('length', type=int,
+                            help='insert the length of the Pormoter')
 
-lenPromoter = Promoter.integers
+parser.add_argument('genoma', type=str,
+                            help='genoma sequence in fasta format')
 
-gff = pr.read_gff3(r"/mnt/d/joaon/TCC/Programação_TCC/GFFs/TESTE_CTBE.gff3", as_df=False) #lê o arquivo gff3
+parser.add_argument('gff', type=str,
+                            help='genoma anotation in gff3 format')
+
+parser.add_argument('--fixid', action='store_true', 
+                            help='store true to adjust the gene id adding version to the promoter id')
+parser.add_argument('--v', type=int, action="store",
+                            help='chromosome version in fasta file, use with fixid. Ex: 1,2,3...')
+
+args = parser.parse_args()
+
+genoma = args.genoma
+gff = args.gff
+lenPromoter = args.length
+versao=args.v
+
+#conferir se o arquivo existe no disco
+
+gff = pr.read_gff3(gff, as_df=False) #lê o arquivo gff3
 
 gffgeneplus=gff[(gff.Feature == "gene") & (gff.Strand == "+")] #separa fitas positivas
 gffgeneplus2=gffgeneplus.assign("PromoterStart", lambda x: x.Start-lenPromoter)
@@ -46,10 +62,13 @@ for idpromoter in regiaopromotora.ID:
         regiaopromotora = pr.concat([regiaopromotora, promotorcorreto])
     IDanterior=idpromoter
 
+if args.fixid:
+        regiaopromotora.Chromosome = regiaopromotora.Chromosome.astype(str) + (f'.{versao}')
+
 #Escrevendo arquivo fasta com as regiões promotoras
 with open ("TesteFastaSeqPromotora.txt","w") as TesteFastaSeqPromotora:
     for ID in regiaopromotora.ID:
-        seqID = pr.get_fasta(regiaopromotora[regiaopromotora.ID == ID], "/mnt/d/joaon/TCC/Programação_TCC/Genomas/TESTE_GCA_CTBE.fna")
+        seqID = pr.get_fasta(regiaopromotora[regiaopromotora.ID == ID], genoma)
         for line in seqID:
             TesteFastaSeqPromotora.write(f'>{ID}\n{line}\n')
         
