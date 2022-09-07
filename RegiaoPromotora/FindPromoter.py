6e60fcc1b762da2ede34d52d7703ab54b648935c
JoaoNovoletti <joao.novoletti@usp.br>
2022-09-07 19:28:12 -0300

    fixing AttributeError from 1-bp close genes

--- RegiaoPromotora/FindPromoter.py
+++ RegiaoPromotora/FindPromoter.py
@@ -63,14 +63,29 @@ print("Arquivo gff importado dentro do pyranges em", datetime.datetime.now()) #F
 sys.stdout.flush()
 
 gffgeneplus=gff[(gff.Feature == "gene") & (gff.Strand == "+")] #separa fitas positivas
-gffgeneplus2=gffgeneplus.assign("PromoterStart", lambda x: x.Start-lenPromoter)
-gffgeneplus2=gffgeneplus2.assign("PromoterEnd", lambda x: x.Start)
+if not gffgeneplus.empty:
+    gffgeneplus2=gffgeneplus.assign("PromoterStart", lambda x: x.Start-lenPromoter)
+    gffgeneplus2=gffgeneplus2.assign("PromoterEnd", lambda x: x.Start)
 
 gffgeneminus=gff[(gff.Feature == "gene") & (gff.Strand == "-")] #separa fitas negativas
-gffgeneminus2=gffgeneminus.assign("PromoterStart", lambda x: x.End)
-gffgeneminus2=gffgeneminus2.assign("PromoterEnd", lambda x: x.End+lenPromoter)
+if not gffgeneminus.empty:
+    gffgeneminus2=gffgeneminus.assign("PromoterStart", lambda x: x.End)
+    gffgeneminus2=gffgeneminus2.assign("PromoterEnd", lambda x: x.End+lenPromoter)
 
-gffgenecomparison=pr.concat([gffgeneplus2, gffgeneminus2])
+#Verificação para ver se o arquivo contém gene em ambas as fitas ou apenas uma delas
+if not gffgeneminus.empty and not gffgeneplus.empty:
+    gffgenecomparison=pr.concat([gffgeneplus2, gffgeneminus2])
+    print("***Há genes em ambas as fitas***")
+
+if gffgeneminus.empty and not gffgeneplus.empty:
+    gffgenecomparison = gffgeneplus2
+    print("***Há genes apenas na fita positiva***")
+
+if not gffgeneminus.empty and gffgeneplus.empty:
+    gffgenecomparison = gffgeneminus2
+    print("***Há genes apenas na fita negativa***")
+
+sys.stdout.flush()
 
 gffgenecomparison.ID = "Promoter_" + gffgenecomparison.ID.astype(str)
 gffgenecomparison.Start=gffgenecomparison.PromoterStart
@@ -94,16 +109,19 @@ for idpromoter in regiaopromotora.ID:
         Pgeneid = idpromoter.replace("Promoter_", "")
         promotorcorreto = reppromoter.nearest(gff[gff.ID == Pgeneid], strandedness="same", how="downstream")
         promotorcorreto = promotorcorreto[promotorcorreto.Distance == 1]
-        promotorcorretoconcat=pr.PyRanges(chromosomes=promotorcorreto.Chromosome, starts=promotorcorreto.Start, ends=promotorcorreto.End, strands=promotorcorreto.Strand) #Necessita-se excluir algumas colunas que são denominadas "*_b", pois há um erro que insere novas colunas em IDs com o mesmo cromossomo
-        promotorcorretoconcat.ID=promotorcorreto.ID
-        promotorcorretoconcat.Source=promotorcorreto.Source
-        promotorcorretoconcat.Feature=promotorcorreto.Feature
-        promotorcorretoconcat.Name=promotorcorreto.Name
-        promotorcorretoconcat.Score=promotorcorreto.Score
-        promotorcorretoconcat.Frame=promotorcorreto.Frame
-        regiaopromotora = pr.concat([regiaopromotora, promotorcorretoconcat])
-    IDanterior=idpromoter
+        if promotorcorreto.empty:
+            print("*****WARNING*****\nID:", idpromoter, "is 1 bp far from the previous gene") #Verifica se há a possibilidade de se obter uma região promotora
+        if not promotorcorreto.empty:
+            promotorcorretoconcat=pr.PyRanges(chromosomes=promotorcorreto.Chromosome, starts=promotorcorreto.Start, ends=promotorcorreto.End, strands=promotorcorreto.Strand) #Necessita-se excluir algumas colunas que são denominadas "*_b", pois há um erro que insere novas colunas em IDs com o mesmo cromossomo
+            promotorcorretoconcat.ID=promotorcorreto.ID
+            promotorcorretoconcat.Source=promotorcorreto.Source
+            promotorcorretoconcat.Feature=promotorcorreto.Feature
+            promotorcorretoconcat.Name=promotorcorreto.Name
+            promotorcorretoconcat.Score=promotorcorreto.Score
+            promotorcorretoconcat.Frame=promotorcorreto.Frame
 
+            regiaopromotora = pr.concat([regiaopromotora, promotorcorretoconcat])
+    IDanterior=idpromoter
 
 print("Overlaps e repetições conferidos e corrigidos em", datetime.datetime.now(), "- Próximo passo: escrever arquivo fasta com as regiões promotoras") #Feedback 5 - Correção dos overlaps e repetições e indicação da próxima etapa
 sys.stdout.flush()
@@ -120,9 +138,11 @@ sys.stdout.flush()
 zero=np.int32(0) #Zero com inteiro no mesmo formato do pyranges para evitar mensagem de erro
 
 zerarpromotor = regiaopromotora[regiaopromotora.Start < 0]
-regiaopromotora = regiaopromotora[regiaopromotora.Start >= 0]
-zerarpromotor.Start = zero
-regiaopromotora = pr.concat([regiaopromotora, zerarpromotor])
+if not zerarpromotor.empty:
+    print("Há promotores a serem zerados")
+    regiaopromotora = regiaopromotora[regiaopromotora.Start >= 0]
+    zerarpromotor.Start = zero
+    regiaopromotora = pr.concat([regiaopromotora, zerarpromotor])
 
 print("  -Início da conversão para o arquivo fasta", datetime.datetime.now())
 sys.stdout.flush()
